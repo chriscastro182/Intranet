ed7813226ee9c7644fe719513e09691ed331b1ef
ChrisCastro182 <25974335+chriscastro182@users.noreply.github.com>
2017-12-13 17:30:21 -0600

    Tablas para SEDENA

---
+++ data/DB/scripts/condiciondecarga.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-12-2017 a las 00:27:46
+-- Versión del servidor: 10.1.25-MariaDB
+-- Versión de PHP: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `intranet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `condiciondecarga`
+--
+
+CREATE TABLE `condiciondecarga` (
+  `idCondicion` int(11) NOT NULL,
+  `condicion` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `condiciondecarga`
+--
+
+INSERT INTO `condiciondecarga` (`idCondicion`, `condicion`) VALUES
+(1, 'En buen estado'),
+(2, 'Empaque aplastado'),
+(3, 'Empaque humedo'),
+(4, 'Con faltante'),
+(5, 'RefrigeraciÃ³n'),
+(6, 'Empaque roto'),
+(7, 'Cinta diferente');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `condiciondecarga`
+--
+ALTER TABLE `condiciondecarga`
+  ADD PRIMARY KEY (`idCondicion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
