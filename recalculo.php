f74e18a559a9e8155854472f95331d1ab2ddbbc0
chriscastro182 <deck.chris.182@gmail.com>
2018-03-05 17:32:04 -0600

    Resumen de recálculo

--- recalculo.php
+++ recalculo.php
@@ -62,80 +62,6 @@ if (isset($_SESSION['Rol_idRol'])==FALSE) {
               </div>
           </div>
           <!-- row -->
-          <div class="row">
-
-            <div class="col-sm-6" >
-              <button class="btn btn-lg btn-info btn-block" data-toggle="collapse" data-target="#demo">¿Qué entra dentro del concepto de Mercancía Especial?</button>
-                <div id="demo" class="collapse">
-                  <div class="container">
-                    <h4>Mercancías Especiales:</h4>
-                    <ul>
-                      <li>a) Las contenidas en cajas, contenedores, cartones, rejas y otros empaques y envases, cuyo volumen
-                        sea de más de 5 metros cúbicos.</li>
-                      <li>b) Las que deban guardarse en cajas fuertes o bajo custodia especial.</li>
-                      <li>c) Las explosivas, inflamables, contaminantes, radioactivas y corrosivas</li>
-                      <li>d) Las que por su naturaleza deban conservarse en refrigeración, en cuartos estériles o
-                          en condiciones especiales dentro de los recintos fiscales.</li>
-                      <li>e) Los animales vivos.</li>
-                    </ul>
-                  </div>
-                </div>
-            </div>
-          </div>
-          <!-- row -->
-          <div class="row">
-              <form  class="form-horizontal" method="POST" action="guardar.php">
-                <!-- Aquí van los campos -->
-                <table class="table table-bordered table-condensed">
-                      <thead> <?php $hoy = date('d-m-Y'); ?>
-                        <tr>
-                          <th>Expediente</th>
-                          <th>Clave única</th>
-                          <th>Fecha de <br> Ingreso</th>
-                          <th>GuíaMaster</th>
-                          <th>GuíaHouse</th>
-                          <th>Piezas</th>
-                          <th>Peso</th>
-                          <th>Descripcion</th>
-                          <th>Fecha de <br> Salida <?php echo $hoy; ?></th>
-                          <th>Estatus</th>
-                          <th>Tipo de mercancía</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                          <tr>
-                             <td><input type="text" id="expediente" name="expediente" class="form-control" required/></td>
-                             <td><input type="text" id="claveUnica" name="claveUnica" class="form-control" required/></td>
-                             <td><input type="date" id="ingreso" name="ingreso" class="form-control" required/></td>
-                             <td><input type="text" id="guiaMaster" name="guiaMaster" class="form-control" required/></td>
-                             <td><input type="text" id="guiaHouse" name="guiaHouse" class="form-control" required/></td>
-                             <td><input type="number" id="piezas" name="piezas"class="form-control" min="1" required/></td>
-                             <td><input type="text" name="peso" class="form-control" id="peso" required></td>
-                             <td><input type="text" id="descripcion" name="descripcion" class="form-control"/></td>
-                             <td><input type="date" id="salida" name="salida" max="<?php echo $hoy; ?>" class="form-control" required/></td>
-
-                             <td>
-                               <select class="form-control" id="estatus" name="estatus">
-                                  <option value="Inactivo">Inactivo</option>
-                                  <option value="Activo">Activo</option>
-                    						</select>
-                              <td>
-                                <select class="form-control" id="excepcion" name="excepcion" >
-                                   <option value="Normal">Normal</option>
-                                   <option value="Especial">Especial</option>
-                                   <option value="Efectos Personales">Efectos Personales</option>
-                                </select>
-                              </td>
-                          </tr>
-                      </tbody>
-                    </table>
-          </div>
-          <div class="col-sm-3 col-md-3 col-lg-3 pull-right">
-            <input type="hidden" name="idOficio" value="<?php echo $idOficio; ?>">
-            <button class="btn  btn-primary btn-lg" type="submit"><i class="far fa-save "></i>  Calcular y guardar</button>
-          </div>
-            </form>
-          <!-- row -->
           <div class="row" <?php echo $mostrar; ?>>
             <div class="col-sm-12 col-md-12 col-lg-12">
                   <table class="table table-striped">
@@ -162,7 +88,7 @@ if (isset($_SESSION['Rol_idRol'])==FALSE) {
                       $fUno=strtotime($rowRegistro['f_ingreso']);
                       $fDos=strtotime($rowRegistro['f_salida']);
                       $diasTotales=ceil(abs($fDos - $fUno) / 86400);  //función que calcula la diferencia en días entre
-                      
+
                       $derechos3=0;
                       $derechos2=0;
                       $derechos1=0;
@@ -197,13 +123,13 @@ if (isset($_SESSION['Rol_idRol'])==FALSE) {
                           <td><?php echo $rowRegistro['diasTotales']; ?></td>
                           <td><?php echo $rowRegistro['estatus']; ?></td>
                           <td><?php echo $rowRegistro['derechos']; ?></td>
-                          <td><?php echo $derechos1+$derechos2+$derechos3; ?></td>
+                          <td style="background-color:#FFCC00;"><?php echo $derechos1+$derechos2+$derechos3; ?></td>
                           <td><?php echo $rowRegistro['excepcion']; ?></td>
                         </tr>
                         <?php } ?>
                     </tbody>
                   </table>
-                  <a href="informeExcel.php?oficio=<?php echo $idOficio; ?>" class="btn btn-success btn-block" type="button" name="excel"><i class="far fa-file-excel fa-2x"></i> Descargar informe en EXCEL</a>
+
               </div>
           </div>
           <!-- footer -->
